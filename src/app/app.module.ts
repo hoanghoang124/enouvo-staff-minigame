187a5166f96cebb676fe8612167b43d46d4aa856
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-02-20 17:52:20 +0700

    * Deploy login, staff site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { DataTablesModule } from "angular-datatables";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { UserSiteComponent } from "./user-site/user-site.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, UserSiteComponent],
+  imports: [BrowserModule, AppRoutingModule, DataTablesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/user-site/user-site.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-user-site",
+  templateUrl: "./user-site.component.html",
+  styleUrls: ["./user-site.component.css"]
+})
+export class UserSiteComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
