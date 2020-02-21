c98a5ab7d00f3e1aace25573956db1448838b963
tri <rinthu1997@gmail.com>
2020-02-21 17:55:23 +0700

    + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserSiteComponent } from './user-site/user-site.component';
+import { AdminSiteComponent } from './admin-site/admin-site.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'user', component: UserSiteComponent},
   {path: 'admin', component: AdminSiteComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
