f0bf16b48a32e97f63d132e4bdb93976ba5634dd
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-02-21 11:44:42 +0700

    * commit to pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { UserSiteComponent } from "./user-site/user-site.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, UserSiteComponent],
-  imports: [BrowserModule, AppRoutingModule, DataTablesModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DataTablesModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
