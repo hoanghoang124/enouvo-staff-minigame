9c2ca33118fe50df94e756353ed0f098578d8528
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-02-26 14:41:04 +0700

    * auth guard, divide role admin, user successfull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,10 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { fakeBackendProvider } from "./login/_helpers/fake-backend";
 import { AppComponent } from "./app.component";
 import { appRoutingModule } from "./app-routing.module";
-
-import { DataTablesModule } from "angular-datatables";
 import { CommonModule } from "@angular/common";
-import { BasicAuthInterceptor } from "./login/_helpers/basic-auth.interceptor";
 import { ErrorInterceptor } from "./login/_helpers/error.interceptor";
 import { LoginComponent } from "./login/login.component";
 import { UserSiteComponent } from "./user-site/user-site.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AdminSiteComponent } from "./admin-site/admin-site.component";
 import {
   MatInputModule,
