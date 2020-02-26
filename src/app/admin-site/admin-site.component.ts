a46af5599b9af5b85eeb9c70fbb2f6648d0a98b2
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-02-26 10:38:19 +0700

    * authentication works, auth guard divide admin role and user role works

--- src/app/admin-site/admin-site.component.ts
+++ src/app/admin-site/admin-site.component.ts
@@ -1,34 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { DataTablesModule } from 'angular-datatables';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
-import 'rxjs';
-import { Observable } from 'rxjs';
-import { CommonModule } from '@angular/common';
-import { StaffService } from './staff.service';
-import { Staff } from './staff.model';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { DataTablesModule } from "angular-datatables";
+import { BrowserModule } from "@angular/platform-browser";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders
+} from "@angular/common/http";
+import "rxjs";
+import { Observable } from "rxjs";
+import { CommonModule } from "@angular/common";
+import { StaffService } from "./staff.service";
+import { Staff } from "./staff.model";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-admin-site',
-  templateUrl: './admin-site.component.html',
-  styleUrls: ['./admin-site.component.css']
+  selector: "app-admin-site",
+  templateUrl: "./admin-site.component.html",
+  styleUrls: ["./admin-site.component.css"]
 })
 export class AdminSiteComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'information', 'star'];
+  displayedColumns: string[] = ["name", "information", "star"];
   data: Staff[] = [];
   isLoadingResults = true;
 
-  constructor(private staffService: StaffService) { }
+  constructor(private staffService: StaffService) {}
 
   ngOnInit() {
-    this.staffService.getStaffs()
-      .subscribe(res => {
+    this.staffService.getStaffs().subscribe(
+      res => {
         this.data = res;
         this.isLoadingResults = false;
-      }, err => {
+      },
+      err => {
         console.log(err);
         this.isLoadingResults = false;
-      });
+      }
+    );
   }
 }
