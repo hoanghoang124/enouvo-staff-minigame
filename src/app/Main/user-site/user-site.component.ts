68501f8bde5133875f60ec424cb820da129c2ad4
tri <rinthu1997@gmail.com>
2020-03-05 15:31:38 +0700

    * show dashboard after login

--- src/app/Main/user-site/user-site.component.ts
+++ src/app/Main/user-site/user-site.component.ts
@@ -3,8 +3,8 @@ import { AuthService } from '../../Auth/Services/auth.service';
 import { Component, OnInit, HostListener } from '@angular/core';
 import { StaffService } from '../Services/staff.service';
 import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
-import * as fromApp from '../../Store';
+import { Store, select } from '@ngrx/store';
+import * as fromStaff from '../../Store';
 import { State } from '../../Store/reducers';
 
 export interface Tile {
@@ -30,77 +30,14 @@ export class UserSiteComponent implements OnInit {
   hideScrollHeight = 10;
   currentUser: any;
   userFromApi: any;
-  staffs: any[] = [
-    {
-      id: '1',
-      name: 'name 1',
-      information: 'information 1',
-      star: 22
-    },
-    {
-      id: '2',
-      name: 'name 2',
-      information: 'information 2',
-      star: 96
-    },
-    {
-      id: '3',
-      name: 'name 3',
-      information: 'information 3',
-      star: 3
-    },
-    {
-      id: '4',
-      name: 'name 4',
-      information: 'information 4',
-      star: 1
-    },
-    {
-      id: '5',
-      name: 'name 5',
-      information: 'information 5',
-      star: 17
-    },
-    {
-      id: '6',
-      name: 'name 6',
-      information: 'information 6',
-      star: 68
-    },
-    {
-      id: '7',
-      name: 'name 7',
-      information: 'information 7',
-      star: 25
-    },
-    {
-      id: '8',
-      name: 'name 8',
-      information: 'information 8',
-      star: 46
-    },
-    {
-      id: '9',
-      name: 'name 9',
-      information: 'information 9',
-      star: 87
-    },
-    {
-      id: '10',
-      name: 'name 10',
-      information: 'information 10',
-      star: 85
-    }
-  ];
+  staffs: any = [];
+  stafflist$: Observable<any>;
 
   constructor(
     private staffService: StaffService,
-
     private authService: AuthService,
     private store: Store<State>
-  ) {
-    // this.currentUser = this.AuthGuardService.currentUserValue;
-  }
+  ) {}
 
   showDetail() {
     this.clicked = !this.clicked;
@@ -137,10 +74,15 @@ export class UserSiteComponent implements OnInit {
 
   ngOnInit() {
     this.clicked = false;
-    this.getState.subscribe(state => {
-      this.isAuthenticated = state.isAuthenticated;
-      this.user = state.user;
-      this.errorMessage = state.errorMessage;
+    // this.getState.subscribe(state => {
+    //   this.isAuthenticated = state.isAuthenticated;
+    //   this.user = state.user;
+    //   this.errorMessage = state.errorMessage;
+    // });
+    this.staffService.getStaffs().subscribe((staffs: {}) => {
+      this.staffs = staffs;
     });
+    this.store.dispatch(new fromStaff.GetStaffs());
+    this.stafflist$ = this.store.pipe(select(fromStaff.getAllStaffs));
   }
 }
