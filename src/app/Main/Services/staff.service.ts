173ee98275e7ee5cda59cb6b91dbd725a989c51b
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-03-05 18:01:12 +0700

    * config to deploy

--- src/app/Main/Services/staff.service.ts
+++ src/app/Main/Services/staff.service.ts
@@ -1,4 +1,3 @@
-import { environment } from './../../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
