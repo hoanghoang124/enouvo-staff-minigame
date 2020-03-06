e4c79b3e7ccef8aabaa57d225194dd8def27f286
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-03-06 11:56:29 +0700

    * config to deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 
+app.use(cors());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
