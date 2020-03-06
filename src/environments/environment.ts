096107948359f51544a655e00baa7b75e9857e63
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-03-06 11:34:44 +0700

    * config to deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true
-  // Url: 'https://5e55e20836450d001428865d.mockapi.io/staff'
+  //Url: 'https://5e55e20836450d001428865d.mockapi.io/staff'
 };
 
 /*
