348aade6f27d1bd3e3058a3bb7b38bdd8a2b7e52
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-03-06 11:20:31 +0700

    * config to deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: ture
   // Url: 'https://5e55e20836450d001428865d.mockapi.io/staff'
 };
 
