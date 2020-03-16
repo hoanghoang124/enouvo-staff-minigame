62ffcbe3b8bab4c4e16537d1695c2aab5e5aabac
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-03-16 16:57:34 +0700

    * delete chart in adminsite page

--- src/app/Main/admin-site/admin-site.component.ts
+++ src/app/Main/admin-site/admin-site.component.ts
@@ -5,10 +5,8 @@ import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { State } from 'src/app/Store/reducers';
 import * as fromStaff from '../../Store';
 import { Staff } from '../Models/staff.model';
-import Chart from 'chart.js';
 import { fadeInAnimation } from '../animation/fade-in.animation';
 import * as _ from 'lodash';
-import { StarService } from '../Services/star.service';
 
 @Component({
   selector: 'app-admin-site',
@@ -30,56 +28,9 @@ export class AdminSiteComponent implements OnInit {
     this.data.filter = filterValue.trim().toLowerCase();
   }
 
-  constructor(private store: Store<State>, private star: StarService) {}
+  constructor(private store: Store<State>) {}
 
   ngOnInit() {
-    this.star.getStars().subscribe(res => {
-      const name = _.map(res, 'name');
-      const star = _.map(res, 'star');
-
-      const chart = document.getElementById('myChart');
-      this.myChart = new Chart(chart, {
-        type: 'bar',
-        data: {
-          labels: name,
-          datasets: [
-            {
-              label: 'number of stars',
-              data: star,
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-            }
-          ]
-        },
-        options: {
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true
-                }
-              }
-            ]
-          }
-        }
-      });
-    });
-
     this.store.dispatch(new fromStaff.GetStaffs());
     this.stafflist$ = this.store.select(fromStaff.getAllStaffs);
     this.stafflist$.subscribe(res => {
