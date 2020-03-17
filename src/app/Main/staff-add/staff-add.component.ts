19db90ecabc7e97a8f8a5c7b7f47fda12793e7bc
tri <rinthu1997@gmail.com>
2020-03-17 10:42:19 +0700

    * register form

--- src/app/Main/staff-add/staff-add.component.ts
+++ src/app/Main/staff-add/staff-add.component.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { Staff } from '../Models/staff.model';
 import { Store } from '@ngrx/store';
 import { State } from 'src/app/Store';
-import * as fromStaff from '../../Store';
+import * as appStore from '../../Store';
 
 @Component({
   selector: 'app-staff-add',
@@ -15,12 +15,14 @@ export class StaffAddComponent implements OnInit {
   staffForm: FormGroup;
   isLoadingResults$: Observable<boolean>;
   staff$: Observable<Staff>;
+  hide = true;
 
   constructor(private formBuilder: FormBuilder, private store: Store<State>) {}
 
   ngOnInit() {
     this.staffForm = this.formBuilder.group({
       id: [null, Validators.required],
+      username: [null, Validators.required],
       firstName: [null, Validators.required],
       middleName: [null, Validators.required],
       lastName: [null, Validators.required],
@@ -36,7 +38,27 @@ export class StaffAddComponent implements OnInit {
   }
 
   onFormSubmit() {
-    this.isLoadingResults$ = this.store.select(fromStaff.getIsLoading);
-    this.store.dispatch(new fromStaff.CreateStaff(this.staffForm.value));
+    const register = {
+      username: this.staffForm.get('username').value,
+      email: this.staffForm.get('email').value
+    };
+    const staff: Staff = {
+      id: this.staffForm.get('id').value,
+      firstName: this.staffForm.get('firstName').value,
+      middleName: this.staffForm.get('middleName').value,
+      lastName: this.staffForm.get('lastName').value,
+      avatar: this.staffForm.get('avatar').value,
+      email: this.staffForm.get('email').value,
+      quote: this.staffForm.get('quote').value,
+      birthday: this.staffForm.get('birthday').value,
+      phone: this.staffForm.get('phone').value,
+      addressStreet: this.staffForm.get('addressStreet').value,
+      addressCity: this.staffForm.get('addressCity').value,
+      position: this.staffForm.get('position').value
+    };
+    console.log(register);
+    this.isLoadingResults$ = this.store.select(appStore.getIsLoading);
+    this.store.dispatch(new appStore.Register(register));
+    this.store.dispatch(new appStore.CreateStaff(staff));
   }
 }
