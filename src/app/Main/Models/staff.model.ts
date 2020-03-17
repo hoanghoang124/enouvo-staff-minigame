507e8530c742cfab4e7f9fa68b596186dd691c82
tri <rinthu1997@gmail.com>
2020-03-17 18:04:24 +0700

    * lam lai cuoc doi

--- src/app/Main/Models/staff.model.ts
+++ src/app/Main/Models/staff.model.ts
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-export class Staff {
-=======
-=======
->>>>>>> parent of 7452362... * config to deploy
-=======
->>>>>>> parent of 7452362... * config to deploy
-import { User } from '../../Auth/Models/user.model';
-
 export interface Staff {
-  userId: User['id'];
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 7452362... * config to deploy
-=======
->>>>>>> parent of 7452362... * config to deploy
-=======
->>>>>>> parent of 7452362... * config to deploy
-  id: number;
   firstName: string;
   middleName: string;
   lastName: string;
