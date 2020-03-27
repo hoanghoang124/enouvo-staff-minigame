2a7387fb7e8a59ef420cbb6c0fcdd3e71e3c666c
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-03-27 17:51:56 +0700

    * fixing routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthGuard } from './layouts/auth-layout/services/auth-guard.service';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+// import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 const routes: Routes = [
   {
     path: '',
@@ -23,17 +23,17 @@ const routes: Routes = [
       }
     ]
   },
-  {
-    path: '',
-    component: AuthLayoutComponent,
-    children: [
-      {
-        path: '',
-        loadChildren:
-          './layouts/auth-layout/auth-layout.module#AuthLayoutModule'
-      }
-    ]
-  },
+  // {
+  //   path: '',
+  //   component: AuthLayoutComponent,
+  //   children: [
+  //     {
+  //       path: '',
+  //       loadChildren:
+  //         './layouts/auth-layout/auth-layout.module#AuthLayoutModule'
+  //     }
+  //   ]
+  // },
   {
     path: '**',
     redirectTo: 'dashboard'
