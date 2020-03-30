99139354beffff0d5f5942573e3c42ce7a3b2cf1
tri <rinthu1997@gmail.com>
2020-03-30 08:31:25 +0700

    * change server's path

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/enouvo-staff-minigame'));
+app.use(express.static(__dirname + '/dist/enouvo-staff-minigame-dev'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/enouvo-staff-minigame/index.html'));
+  res.sendFile(
+    path.join(__dirname + '/dist/enouvo-staff-minigame-dev/index.html')
+  );
 });
 
 // Start the app by listening on the default Heroku port
