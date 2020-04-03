cd5eb3b06f4ed2ce0500e3403bdec5f85e7f43da
tri <rinthu1997@gmail.com>
2020-04-03 10:04:29 +0700

    * fix double quote

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,30 +1,30 @@
-import { Routes } from "@angular/router";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { CreateAccountComponent } from "./create-account/create-account.component";
-import { UserProfileComponent } from "./user-profile/user-profile.component";
-import { TablesComponent } from "./tables/tables.component";
-import { AdminGuard } from "../auth-layout/services/role-guard.service";
-import { UploadCSVComponent } from "./upload-csv/upload-csv.component";
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TablesComponent } from './tables/tables.component';
+import { AdminGuard } from '../auth-layout/services/role-guard.service';
+import { UploadCSVComponent } from './upload-csv/upload-csv.component';
 export const AdminLayoutRoutes: Routes = [
   {
-    path: "dashboard",
+    path: 'dashboard',
     component: DashboardComponent
   },
   {
-    path: "dashboard/:id/user-profile",
+    path: 'dashboard/:id/user-profile',
     component: UserProfileComponent
   },
   {
-    path: "tables/:id/user-profile",
+    path: 'tables/:id/user-profile',
     component: UserProfileComponent,
     canActivate: [AdminGuard]
   },
 
-  { path: "tables", component: TablesComponent, canActivate: [AdminGuard] },
-  { path: "upload", component: UploadCSVComponent, canActivate: [AdminGuard] },
+  { path: 'tables', component: TablesComponent, canActivate: [AdminGuard] },
+  { path: 'upload', component: UploadCSVComponent, canActivate: [AdminGuard] },
 
   {
-    path: "create-account",
+    path: 'create-account',
     component: CreateAccountComponent,
     canActivate: [AdminGuard]
   }
--- src/app/layouts/admin-layout/upload-csv/upload-csv.component.ts
+++ src/app/layouts/admin-layout/upload-csv/upload-csv.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { UploadService } from './upload.service';
 
 @Component({
   selector: 'app-upload-csv',
@@ -11,46 +11,35 @@ export class UploadCSVComponent implements OnInit {
   headers = new HttpHeaders({
     Authorization: `Bearer ${localStorage.getItem('token')}`
   });
+  @ViewChild('file') file;
+  canBeClosed = true;
+  primaryButtonText = 'Upload';
+  showCancelButton = true;
+  uploading = false;
+  uploadSuccessful = false;
+  public files: Set<File> = new Set();
+  progress: any;
 
-  SERVER_URL =
-    'https://training-management-dev.herokuapp.com/api/v1/auth/register-by-importing-csv-file';
-  form: FormGroup;
-  formData = new FormData();
-
-  constructor(private fb: FormBuilder, private http: HttpClient) {}
-
-  ngOnInit() {
-    this.form = this.fb.group({
-      file: null
-    });
+  addFiles() {
+    this.file.nativeElement.click();
   }
 
-  uploadFile(event) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.form.get('file').setValue(file);
+  onFilesAdded() {
+    const files: { [key: string]: File } = this.file.nativeElement.files;
+    for (const key in files) {
+      if (!isNaN(parseInt(key))) {
+        this.files.add(files[key]);
+      }
     }
   }
-
-  submitForm() {
-    const formData = new FormData();
-
-    formData.append('file', this.form.get('file').value);
-    this.http
-      .post(this.SERVER_URL, this.formData, {
-        headers: this.headers
-      })
-      .subscribe(
-        res => console.log(res),
-        err => console.log(err)
-      );
-    console.log(this.formData);
-    const request = new XMLHttpRequest();
-    request.open('POST', this.SERVER_URL, true);
-    request.setRequestHeader(
-      'Authorization',
-      `Bearer ${localStorage.getItem('token')}`
-    );
-    request.send(this.formData);
+  closeDialog() {
+    this.progress = this.uploadService.upload(this.files);
+    const allProgressObservables = [];
+    for (const key in this.progress) {
+      allProgressObservables.push(this.progress[key].progress);
+    }
   }
+  constructor(private uploadService: UploadService) {}
+
+  ngOnInit() {}
 }
